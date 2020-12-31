642bef5dc0e2bf3d05504bba49a91a2b27ad52c5
Dominiek Broux <Dominiekbroux@hotmail.com>
2020-12-31 14:51:08 +0100

    Start opgave

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeBib.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
